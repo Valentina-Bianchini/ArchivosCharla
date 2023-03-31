83c9a4d3543631a0583b0306a65222ec6002fd5c
valentina.bianchini <bianchinival19@gmail.com>
2023-03-31 14:45:36 -0300

    primer commit

---
+++ ejemplo.bat
@@ -0,0 +1,32 @@
+@echo off
+
+/*abre vsc con comandos de .bat*/
+/*
+.bat es un archivo batch o ejecutable por lotes, es un archivo de texto plano que contiene una 
+serie de comandos se SO que pueden ejectarse en consola
+*/ 
+
+/*inicia una nueva instancia de editor de código fuente Visual Studio Code*/
+start code "C:\Users\valentina.bianchini\Desktop\ejemplo.bat"
+
+/*detiene la ejecución de un archivo batch o script por un período de tiempo determinado antes 
+de continuar con la ejecución*/
+timeout /t 5
+
+/*inicia una nueva instancia de edge en el servidor local de Jenkins*/
+start microsoft-edge:http://localhost:8080/
+
+/*inicia una nueva instancia de edge con la documentacion de Jenkins*/
+start microsoft-edge:https://www.jenkins.io/doc/
+
+timeout /t 5
+
+/*inicia una nueva instancia de intellij con el proyecto de automatizacion a ejecutar 
+desde Jenkins*/
+
+/*seteo de variables*/
+set IDEA_BIN_DIR="C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.2.3\bin"
+set PROJECT_DIR="C:\Users\valentina.bianchini\Desktop\Cursos Testing\Clarin Web\SerenityCucumberBDD"
+
+/*comando de ejecución de instancia*/
+start "" %IDEA_BIN_DIR%\idea64.exe %PROJECT_DIR%
